c14e06c745ce436c0c8a0bfb00b623e3e59e173d
priya <krishnapriyanalla@gmail.com>
2020-06-21 17:57:01 +0530

    Updates

---
+++ precache-manifest.bec1a90921fecf679d686cb16585c3c7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "562c1292c95a3a00801d539cb5f33083",
+    "url": "/kp/index.html"
+  },
+  {
+    "revision": "ecc47d2b1ff3a8222214",
+    "url": "/kp/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "82861724589ba3b1a462",
+    "url": "/kp/static/js/2.ce6aabe0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/kp/static/js/2.ce6aabe0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ecc47d2b1ff3a8222214",
+    "url": "/kp/static/js/main.db0a315f.chunk.js"
+  },
+  {
+    "revision": "85a14a8f98212718a088",
+    "url": "/kp/static/js/runtime-main.fd79a111.js"
+  }
+]);
